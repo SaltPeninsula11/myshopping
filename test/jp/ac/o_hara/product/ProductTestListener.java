b6b807624978ae9aa6e9fedac843d6ea633634f8
kouta <kouta@192.168.210.236>
2020-12-30 22:54:49 +0900

    商品の追加（果物編）

--- test/jp/ac/o_hara/product/ProductTestListener.java
+++ test/jp/ac/o_hara/product/ProductTestListener.java
@@ -48,16 +48,22 @@ public class ProductTestListener implements ServletContextListener {
 		}
 		//野菜、果物、肉等
 		if (dao.count() <= 0) {
-			dao.create(new ProductBean("test", "テスト", 100, "テスト", "", "", "", ""));
-			dao.create(new ProductBean("strawberry", "苺", 120, "果物", "", "", "", ""));
-			dao.create(new ProductBean("orange", "みかん", 120, "果物", "", "", "", ""));
-			dao.create(new ProductBean("lemon", "レモン", 160, "果物", "", "", "", ""));
-			dao.create(new ProductBean("grapefruit", "グレープフルーツ", 200, "果物", "", "", "", ""));
-			dao.create(new ProductBean("apple", "りんご", 300, "果物", "", "", "", ""));
-			dao.create(new ProductBean("grapes", "ぶどう", 300, "果物", "", "", "", ""));
-			dao.create(new ProductBean("muscat", "マスカット", 350, "果物", "", "", "", ""));
-			dao.create(new ProductBean("cherry", "さくらんぼ", 700, "果物", "", "", "", ""));
-			dao.create(new ProductBean("melon", "メロン", 1000, "果物", "", "", "", ""));
+			//dao.create(new ProductBean("test", "テスト", 100, "テスト", "", "", "", ""));
+			dao.create(new ProductBean("strawberry", "苺", 120, "果物", "冬", "春", "", ""));
+			dao.create(new ProductBean("kiwi", "キウイ", 120, "果物", "秋", "", "", ""));
+			dao.create(new ProductBean("orange", "みかん", 120, "果物", "冬", "春", "", ""));
+			dao.create(new ProductBean("banana", "バナナ", 130, "果物", "夏", "", "", ""));
+			dao.create(new ProductBean("lemon", "レモン", 160, "果物", "秋", "冬", "", ""));
+			dao.create(new ProductBean("grapefruit", "グレープフルーツ", 200, "果物", "春", "", "", ""));
+			dao.create(new ProductBean("apple", "りんご", 300, "果物", "秋", "冬", "", ""));
+			dao.create(new ProductBean("grapes", "ぶどう", 300, "果物", "秋", "", "", ""));
+			dao.create(new ProductBean("muscat", "マスカット", 350, "果物", "夏", "秋", "", ""));
+			dao.create(new ProductBean("pineapple", "パイナップル", 400, "果物", "春", "夏", "", ""));
+			dao.create(new ProductBean("peach", "桃", 500, "果物", "夏", "", "", ""));
+			dao.create(new ProductBean("cherry", "さくらんぼ", 700, "果物", "夏", "", "", ""));
+			dao.create(new ProductBean("melon", "メロン", 1000, "果物", "夏", "", "", ""));
+			dao.create(new ProductBean("watermelon", "すいか", 1000, "果物", "夏", "", "", ""));
+			dao.create(new ProductBean("candymelon", "キャンディメロン", 1500, "果物", "", "", "", ""));
 		}
 
 		System.out.println("SystemStart..");
